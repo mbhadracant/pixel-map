06deb369c1f0977db450c2ad6bf5c7bdf52ee9ca
mbhadracant <mbhadracant@gmail.com>
2017-09-05 02:14:24 +0100

    pixel map can be zoomed and dragged

--- app/javascripts/pixel.js
+++ app/javascripts/pixel.js
@@ -3,10 +3,25 @@ function Pixel(x , y, i, j) {
   this.y = y;
   this.i = i;
   this.j = j;
+
   this.col = color(Math.floor(random(250)), Math.floor(random(250)), Math.floor(random(250)));
 
   this.onHover = function() {
-    if(mouseX > this.x && mouseY > this.y && mouseX < this.x + pixelSize && mouseY < this.y + pixelSize) {
+    var offsetX = (600/zoom);
+    offsetX = (600 - offsetX)/2;
+    offsetX = this.x - offsetX;
+    offsetX = offsetX * zoom;
+    offsetX -= (pixelSize * zoom)/2;
+
+    var offsetY = (600/zoom);
+    offsetY = (600 - offsetY)/2;
+    offsetY = this.y - offsetY;
+    offsetY = offsetY * zoom;
+    offsetY -= (pixelSize * zoom)/2;
+
+    var pSize = zoom * pixelSize;
+
+    if(mouseX - (tX - width/2) > offsetX && mouseY - (tY - height/2) > offsetY && mouseX - (tX - width/2) < offsetX + pSize && mouseY - (tY - height/2) < offsetY + pSize) {
       fill(this.col.levels[0] + 30,this.col.levels[1] + 30,this.col.levels[2] + 30);
       $(".point").html("X: " + this.i + " Y: " + this.j);
       $(".r").html("R: " + this.col.levels[0]);
